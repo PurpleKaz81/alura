602c5d039d31d835eae4c665a2c359d109f74a41
Rafael Kasinski <rakasin@gmail.com>
2022-12-22 16:54:20 -0300

    Finished ball and racket basics

---
+++ pong.js
@@ -0,0 +1,21 @@
+// ball variables
+let ballX = 300
+let ballY = 200
+let ballDiameter = 15
+let ballRadius = ballDiameter / 2
+
+// ball speed
+let ballVelocityX = 6
+let ballVelocityY = 6
+
+// racket variables (p1R and p2R)
+p1RY = 750
+p2RY = 750
+
+// score variables
+p1Score = 0
+p2Score = 0
+
+const draw = () => {
+  // draw ball and rackets
+}
