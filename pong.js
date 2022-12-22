cdf97593394488e5dd427f31d6e56c3994b47317
Rafael Kasinski <rakasin@gmail.com>
2022-12-22 20:57:35 -0300

    Reset variables and started work on keydown/up events

--- pong.js
+++ pong.js
@@ -9,3 +9,22 @@ let ballRadius = 10
 let ballVelocityX = 5
 let ballVelocityY = 5
 
+// set up paddles
+let paddleHeight = 75
+let paddleWidth = 10
+let leftPaddleY = (canvas.height - paddle.height) / 2
+let rightPaddleY = (canvas.height - paddle.height) / 2
+
+// set up score
+let leftPaddleScore = 0
+let rightPaddleScore = 0
+
+// set up keyboard controls
+let upPressed = false
+let downPressed = false
+let wPressed = false
+let sPressed = false
+
+document.addEventListener("keydown", keyDownHandler)
+document.addEventListener("keyup", keyUpHandler)
+
