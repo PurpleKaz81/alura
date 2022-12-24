16e380983a590cb7f8f96a68823f6795048a6aeb
Rafael Kasinski <rakasin@gmail.com>
2022-12-24 18:28:06 -0300

    Tidied up the code

--- pong/pong.js
+++ pong/pong.js
@@ -25,7 +25,6 @@ let gameLoop
 function draw() {
   // clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  console.log('canvas cleared')
 
   // draw ball
   ctx.beginPath()
@@ -33,48 +32,47 @@ function draw() {
   ctx.fillStyle = "#fff"
   ctx.fill()
   ctx.closePath()
-  console.log('ball created')
 
   // draw left paddle
   ctx.fillStyle = "#fff"
   ctx.fillRect(0, leftPaddleY, paddleWidth, paddleHeight)
-  console.log('left paddle drawn')
 
   // draw right paddle
   ctx.fillStyle = "#fff"
   ctx.fillRect(canvas.width - paddleWidth, rightPaddleY, paddleWidth, paddleHeight)
-  console.log('right paddle drawn')
 
   // draw scores
   ctx.font = "48px sans-serif"
   ctx.textAlign = "center"
   ctx.fillText(leftScore, canvas.width * 1 / 4, 50)
   ctx.fillText(rightScore, canvas.width * 3 / 4, 50)
-  console.log('scores drawn')
 
   // update ball position
   ballX += ballVelocityX
   ballY += ballVelocityY
-  console.log('ball position updated')
 
-  // check for ball collision with paddles
-  if (ballX - ballRadius < paddleWidth) {
-    if (ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) {
-      ballVelocityX = -ballVelocityX
-      ballVelocityY = (ballY - (leftPaddleY + paddleHeight / 2)) * 0.35
-    } else {
-      rightScore++
-      ballReset()
-    }
-    if (ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight) {
-      ballVelocityX = -ballVelocityX
-      ballVelocityY = (ballY - (rightPaddleY + paddleHeight / 2)) * 0.35
-    } else {
-      leftScore++
-      ballReset()
-    }
-    console.log('checked for ball collision with paddles')
+// Check for ball collision with left paddle
+if (ballX - ballRadius < paddleWidth) {
+  if (ballY > leftPaddleY && ballY < leftPaddleY + paddleHeight) {
+    ballVelocityX = -ballVelocityX;
+    ballVelocityY = (ballY - (leftPaddleY + paddleHeight / 2)) * 0.35;
+  } else {
+    rightScore++;
+    ballReset();
   }
+}
+
+// Check for ball collision with right paddle
+if (ballX + ballRadius > canvas.width - paddleWidth) {
+  if (ballY > rightPaddleY && ballY < rightPaddleY + paddleHeight) {
+    ballVelocityX = -ballVelocityX;
+    ballVelocityY = (ballY - (rightPaddleY + paddleHeight / 2)) * 0.35;
+  } else {
+    leftScore++;
+    ballReset();
+  }
+}
+
 
   // check for ball collision with top and bottom
   if (ballY - ballRadius < 0 || ballY + ballRadius > canvas.height) {
@@ -121,6 +119,14 @@ function stopGame() {
 // reset game
 function resetGame() {
   stopGame()
+
+  // reset ball, paddles, and score
+  ballX = 0
+  ballY = 0
+  ballVelocityX = initialBallVelocityX
+  ballVelocityY = initialBallVelocityY
+  leftPaddleY = canvas.height / 2 - paddleHeight
+  rightPaddleY = canvas.height / 2 - paddleHeight
 }
 
 // add event pertinent listeners: keydown and resizing window
