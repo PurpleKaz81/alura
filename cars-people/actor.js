1e17b8abdbd45fc10f273ac37798a0ee4f7f4dba
Rafael Kasinski <rakasin@gmail.com>
2023-01-10 11:37:49 -0300

    Finished images.js

---
+++ cars-people/actor.js
@@ -0,0 +1,7 @@
+// actor starting position
+let xActor = 100
+let yActor = 366
+
+function showActor() {
+  image(actorImage, xActor, yActor, 30, 30)
+}
---
+++ cars-people/images.js
@@ -0,0 +1,14 @@
+// game images
+let roadImage
+let actorImage
+let car1Image
+let car2Image
+let car3Image
+
+function preload() {
+  roadImage = loadImage("images/road.png")
+  actorImage = loadImage("images/player-1.png")
+  car1Image = loadImage("images/car-1.png")
+  car2Image = loadImage("images/car-2.png")
+  car3Image = loadImage("images/car-2.png")
+}
