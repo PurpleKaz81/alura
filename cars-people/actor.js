d7d2cd3fd656e3a17228e8ce524ac0f8650c0edc
Rafael Kasinski <rakasin@gmail.com>
2023-01-10 11:50:54 -0300

    Finished actor js

--- cars-people/actor.js
+++ cars-people/actor.js
@@ -1,7 +1,16 @@
 // actor starting position
 let xActor = 100
-let yActor = 366
+let yActor = 369
 
 function showActor() {
   image(actorImage, xActor, yActor, 30, 30)
+}
+
+function moveActor() {
+  if (keyIsDown(UP_ARROW)) {
+    yActor -= 3
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yActor += 3
+  }
 }
