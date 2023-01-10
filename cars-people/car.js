90e20198d726281ede8f07c8502c953a1f67a838
Rafael Kasinski <rakasin@gmail.com>
2023-01-10 13:41:57 -0300

    Fixed indentation

--- cars-people/car.js
+++ cars-people/car.js
@@ -5,18 +5,18 @@ let carsSpeed = [2, 2.5, 3]
 
 function showCar() {
   for (let i = 0; i < carImages.length; i += 1) {
-  image(carImages[i], xCars[i], yCars[i], 50, 40)
-}
-}
+    image(carImages[i], xCars[i], yCars[i], 50, 40)
+    }
+  }
 
 function moveCar() {
   for (let i = 0; i < carImages.length; i += 1)
     xCars[i] -= carsSpeed[i]
-}
+  }
 
 function initCarPosition() {
   for (i = 0; i < carImages.length; i += 1)
-  if (xCars[i] < -50) {
-    xCars[i] = 600
+    if (xCars[i] < -50) {
+      xCars[i] = 600
+    }
   }
-}
