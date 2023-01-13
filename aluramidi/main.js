8e134fffa3ae167a6cb9db5f63b444cbb1ad809b
Rafael Kasinski <rakasin@gmail.com>
2023-01-13 00:58:02 -0300

    Dynamic class list added

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -7,6 +7,8 @@ const buttonList = document.querySelectorAll(".tecla")
 let counter = 0
 
 while (counter < buttonList.length) {
+  const instrument = buttonList[counter].classList[1]
+  console.log(instrument)
   buttonList[counter].onclick = function () {
     playSound("#som_tecla_tom")
   }
