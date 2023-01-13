b999d8f3b8c2da94e145dbe7ada54f4f84058ee5
Rafael Kasinski <rakasin@gmail.com>
2023-01-12 22:22:35 -0300

    Working on pom sound

--- aluramidi/desafio1/desafio1.js
+++ aluramidi/desafio1/desafio1.js
@@ -3,3 +3,6 @@ document.querySelector("input[type='tel']")
 
 // select button 1
 document.querySelector(".teclado input[value='1']")
+
+// play pom sound
+document.querySelector("#som_tecla_pom").play()
