621156885928323697610d67073aca17014d3ab7
Rafael Kasinski <rakasin@gmail.com>
2023-01-16 21:50:08 -0300

    Further refacotring using closures

--- aluramidi/desafio3/main.js
+++ aluramidi/desafio3/main.js
@@ -3,31 +3,32 @@ const telInput = document.querySelector('input[type=tel]')
 telInput.tabIndex = -1
 telInput.focus()
 
-for (let index = 0; index < buttonList.length; index++) {
-  const button = buttonList[index];
-  button.tabIndex = index + 1
-
+function setupButtonEventListeners(button) {
   button.addEventListener("mousedown", () => button.classList.add("ativa"))
   button.addEventListener("mouseup", () => button.classList.remove("ativa"))
-  button.addEventListener("keydown", (e) => {
-    if (e.key === " " || e.key === "Enter")
-    button.classList.add("ativa")
+  button.addEventListener("keydown", e => {
+    if (e.key === " " || e.key === "Enter") button.classList.add("ativa")
   })
   button.addEventListener("keyup", () => button.classList.remove("ativa"))
   button.addEventListener("click", () => telInput.value += button.value)
 }
 
+for (let index = 0; index < buttonList.length; index++) {
+  const button = buttonList[index]
+  button.tabIndex = index + 1
+  setupButtonEventListeners(button)
+}
+
 const firstButton = buttonList[0]
-firstButton.addEventListener("keydown", (e) => {
+firstButton.addEventListener("keydown", e => {
   if (e.key === "Tab" && e.shiftKey) {
     e.preventDefault()
     telInput.focus()
   }
 })
 
-telInput.addEventListener("keydown", (e) => {
-  if (e.key === " " || e.key === "Enter")
-    telInput.classList.add("ativa")
+telInput.addEventListener("keydown", e => {
+  if (e.key === " " || e.key === "Enter") telInput.classList.add("ativa")
 });
 
 telInput.addEventListener("keyup", () => telInput.classList.remove("ativa"))
