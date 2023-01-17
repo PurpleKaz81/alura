c5eb728293f650e647efb78065213371c512ae2f
Rafael Kasinski <rakasin@gmail.com>
2023-01-16 22:19:47 -0300

    Further refactoring

--- aluramidi/desafio3/main.js
+++ aluramidi/desafio3/main.js
@@ -1,45 +1,40 @@
-const buttonList = document.querySelectorAll('input[type=button]')
-const telInput = document.querySelector('input[type=tel]')
-telInput.tabIndex = -1
-telInput.focus()
+const buttonList = document.querySelectorAll('input[type=button]');
+const telInput = document.querySelector('input[type=tel]');
+telInput.tabIndex = -1;
+telInput.focus();
 
-function setupButtonEventListeners(button) {
-  const events = [
-    {event: "mousedown", action: () => button.classList.add("ativa")},
-    {event: "mouseup", action: () => button.classList.remove("ativa")},
-    {event: "keydown", action: (e) => {
-      if (e.key === " " || e.key === "Enter") button.classList.add("ativa")
+const addEventListeners = (element, events) => {
+    events.forEach(({event, action}) => {
+        element.addEventListener(event, action);
+    });
+};
+
+const buttonEvents = [
+    {event: "mousedown", action: e => e.target.classList.add("ativa")},
+    {event: "mouseup", action: e => e.target.classList.remove("ativa")},
+    {event: "keydown", action: e => {
+        if (e.key === " " || e.key === "Enter") e.target.classList.add("ativa")
     }},
-    {event: "keyup", action: () => button.classList.remove("ativa")},
-    {event: "click", action: () => telInput.value += button.value},
-  ];
-
-  events.forEach(({event, action}) => {
-    button.addEventListener(event, action);
-  });
-}
-
-for (let index = 0; index < buttonList.length; index++) {
-  const button = buttonList[index]
-  button.tabIndex = index + 1
-  setupButtonEventListeners(button)
-}
-
-const firstButton = buttonList[0]
-firstButton.addEventListener("keydown", e => {
-  if (e.key === "Tab" && e.shiftKey) {
-    e.preventDefault()
-    telInput.focus()
-  }
-})
-
-const telEvents = [
-  {event: "keydown", action: e => {
-    if (e.key === " " || e.key === "Enter") telInput.classList.add("ativa")
-  }},
-  {event: "keyup", action: () => telInput.classList.remove("ativa")},
+    {event: "keyup", action: e => e.target.classList.remove("ativa")},
+    {event: "click", action: e => telInput.value += e.target.value},
 ];
 
-telEvents.forEach(({event, action}) => {
-    telInput.addEventListener(event, action);
+const telEvents = [
+    {event: "keydown", action: e => {
+        if (e.key === " " || e.key === "Enter") telInput.classList.add("ativa")
+    }},
+    {event: "keyup", action: e => telInput.classList.remove("ativa")},
+];
+
+buttonList.forEach((button, index) => {
+    button.tabIndex = index + 1;
+    addEventListeners(button, buttonEvents);
 });
+
+const firstButton = buttonList[0];
+firstButton.addEventListener("keydown", e => {
+    if (e.key === "Tab" && e.shiftKey) {
+        e.preventDefault();
+        telInput.focus();
+    }
+  })
