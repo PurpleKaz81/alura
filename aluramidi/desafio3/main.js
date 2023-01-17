ff4d02e9131a2abcc92c5edd16a06510145b0804
Rafael Kasinski <rakasin@gmail.com>
2023-01-16 21:28:03 -0300

    Finished desafio3

--- aluramidi/desafio3/main.js
+++ aluramidi/desafio3/main.js
@@ -1,32 +1,37 @@
 const buttonList = document.querySelectorAll('input[type=button]')
 const telInput = document.querySelector('input[type=tel]')
+telInput.tabIndex = -1
 telInput.focus()
 
 for (index = 0; index < buttonList.length; index++) {
-
   const button = buttonList[index];
+  button.tabIndex = index + 1
 
   button.addEventListener("mousedown", function() {
     button.classList.add("ativa")
   })
-
   button.addEventListener("mouseup", function() {
     button.classList.remove("ativa")
   })
-
   button.addEventListener("keydown", function(e) {
     if (e.key === " " || e.key === "Enter")
     button.classList.add("ativa")
   })
-
   button.addEventListener("keyup", function() {
     button.classList.remove("ativa")
   })
-
   button.addEventListener("click", function() {
     telInput.value += button.value
   })
 }
+
+const firstButton = buttonList[0]
+firstButton.addEventListener("keydown", function(e) {
+  if (e.key === "Tab" && e.shiftKey) {
+    e.preventDefault()
+    telInput.focus()
+  }
+})
 
 telInput.addEventListener("keydown", function(e) {
   if (e.key === " " || e.key === "Enter")
