466b0d10183c5b43b9c7bb7d8c2d5c0d00c673d5
Rafael Kasinski <rakasin@gmail.com>
2023-01-18 19:02:09 -0300

    Better formatting

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,34 +1,36 @@
 fetch("audioIds.json")
-.then(response => response.json())
-.then(data => {
-  const buttonList = document.querySelectorAll(".tecla")
+  .then((response) => response.json())
+  .then((data) => {
+    const buttonList = document.querySelectorAll(".tecla");
 
-  for (let counter = 0; counter < buttonList.length; counter++) {
-    const button = buttonList[counter]
-    const instrument = button.classList[1]
-    let audioId = data[instrument]
+    for (let counter = 0; counter < buttonList.length; counter++) {
+      const button = buttonList[counter];
+      const instrument = button.classList[1];
+      let audioId = data[instrument];
 
-    button.onclick = () => {
-      try {
-        let audioElement = document.querySelector(audioId)
-        audioElement.play()
-      } catch (e) {
-        console.error(`Error: ${e}`)
-      }
+      button.onclick = () => {
+        try {
+          let audioElement = document.querySelector(audioId);
+          audioElement.play();
+        } catch (e) {
+          console.error(`Error: ${e}`);
+        }
+      };
+
+      button.onkeydown = (e) => {
+        if (e.code === "Space" || e.code === "Enter") {
+          button.classList.add("ativa");
+        }
+      };
+
+      button.onkeyup = () => {
+        button.classList.remove("ativa");
+      };
     }
-
-    button.onkeydown = (e) => {
-      if (e.code === "Space" || e.code === "Enter") {
-        button.classList.add("ativa")
-      }
-    }
-
-    button.onkeyup = () => {
-      button.classList.remove("ativa")
-    }
-  }
-})
-.catch(error => {
-  console.error(`An error has occurred with ${error}`)
-  alert(`Error playing the ${error} sound. Please check the audio file and try again.`)
-})
+  })
+  .catch((error) => {
+    console.error(`An error has occurred with ${error}`);
+    alert(
+      `Error playing the ${error} sound. Please check the audio file and try again.`
+    );
+  });
