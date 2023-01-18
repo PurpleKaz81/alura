dde97e2c408739f5a949cfb75384d951854cd780
Rafael Kasinski <rakasin@gmail.com>
2023-01-18 19:35:47 -0300

    Refactored code to handle erros better

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,36 +1,37 @@
+function handleError(error) {
+  console.error(`Error: ${error}`)
+  alert(`Error playing the sound. Please check the audio file and try again.`)
+}
+
 fetch("audioIds.json")
   .then((response) => response.json())
   .then((data) => {
-    const buttonList = document.querySelectorAll(".tecla");
+    const buttonList = document.querySelectorAll(".tecla")
 
-    buttonList.forEach(button => {
-      const instrument = button.classList[1];
-      let audioId = data[instrument];
+    buttonList.forEach((button) => {
+      const instrument = button.classList[1]
+      let audioId = data[instrument]
 
       button.onclick = () => {
-          try {
-              let audioElement = document.querySelector(audioId);
-              audioElement.play();
-          } catch (e) {
-              console.error(`Error: ${e}`);
-          }
-      };
+        try {
+          let audioElement = document.querySelector(audioId)
+          audioElement.play()
+        } catch (error) {
+          handleError(error)
+        }
+      }
 
-      button.onkeydown = (e) => {
-          if (e.code === "Space" || e.code === "Enter") {
-              button.classList.add("ativa");
-          }
-      };
+      button.addEventListener("keydown", (e) => {
+        if (e.code === "Space" || e.code === "Enter") {
+          button.classList.add("ativa")
+        }
+      })
 
-      button.onkeyup = () => {
-          button.classList.remove("ativa");
-      };
-  });
-
+      button.addEventListener("keyup", () => {
+        button.classList.remove("ativa")
+      })
+    })
   })
   .catch((error) => {
-    console.error(`An error has occurred with ${error}`);
-    alert(
-      `Error playing the ${error} sound. Please check the audio file and try again.`
-    );
-  });
+    handleError(error)
+  })
