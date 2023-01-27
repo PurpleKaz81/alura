e8f1247a0a8e155fe0e9febe80c15c550a520746
Rafael Kasinski <rakasin@gmail.com>
2023-01-27 14:26:10 -0300

    Font end is looking ok

--- 7-days-of-code/day3/day3.js
+++ 7-days-of-code/day3/day3.js
@@ -1,5 +1,8 @@
-const gif = document.createElement("img")
-gif.src = "ts-life.gif"
+function openingGif() {
+	const gif = document.createElement("img")
+	gif.src = "ts-life.gif"
+	let gifContainer = document.getElementById("gif-container")
+	gifContainer.appendChild(gif)
+}
 
-let gifContainer = document.getElementById("gif-container")
-gifContainer.appendChild(gif)
+openingGif()
