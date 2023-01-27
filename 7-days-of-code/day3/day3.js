9f4be71fd72061f0a1c558271efe279331534d60
Rafael Kasinski <rakasin@gmail.com>
2023-01-27 18:00:57 -0300

    Using handleInput() for user's name

--- 7-days-of-code/day3/day3.js
+++ 7-days-of-code/day3/day3.js
@@ -1,8 +1,22 @@
 function openingGif() {
-	const gif = document.createElement("img")
-	gif.src = "ts-life.gif"
-	let gifContainer = document.getElementById("gif-container")
-	gifContainer.appendChild(gif)
+  const gif = document.createElement("img")
+  gif.src = "ts-life.gif"
+  let gifContainer = document.getElementById("gif-container")
+  gifContainer.appendChild(gif)
+}
+
+function capitalizeName(name) {
+  return name.replace(/\b(\w)/g, s => s.toUpperCase())
+}
+
+function handleInput() {
+  let fullName = window.prompt("What's you full name, champ?")
+  if (fullName.match(/^[a-zA-Z-'\s]+$/) && fullName != "") {
+    alert(`Thanks, ${capitalizeName(fullName)}!`)
+  } else {
+    alert("Do us a solid and write in a valid name, letters and spaces only")
+  }
 }
 
 openingGif()
+handleInput()
