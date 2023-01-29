6d8bdef1bd18671394f9ae18ef47b02b83070cb0
Rafael Kasinski <rakasin@gmail.com>
2023-01-29 20:22:26 -0300

    Finished question 2 JS code

--- 7-days-of-code/day3/day3.js
+++ 7-days-of-code/day3/day3.js
@@ -41,6 +41,23 @@ function handleInput() {
     alert("No need to lie 😝 Just jot down your real age")
     return handleInput()
   }
+
+  let question1Answer = document.getElementById("question1-answer")
+  let question1 = window.prompt("Would you like to study front-end or back-end? Pick one")
+  question1Answer.innerHTML = question1
+
+  let question2Container = document.querySelector(".question2-container")
+  let question2Answer = document.querySelector("#question2-answer")
+  let question2
+  if (question1.toLowerCase() === "front-end") {
+    question2 = window.prompt("Which framework would you prefer: React or Vue? Pick one")
+  } else if (question1.toLowerCase() === "back-end") {
+    question2 = window.prompt("Which language would you prefer: C++ or Java? Pick one")
+  } else {
+    alert("Please input a valid answer")
+    return handleInput()
+  }
+  question2Answer.innerHTML = question2
 }
 
 document.addEventListener("DOMContentLoaded", openingGif())
