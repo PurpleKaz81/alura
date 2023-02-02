b5a35dfe83e8f35909da661726e4f363a4f576a1
Rafael Kasinski <rakasin@gmail.com>
2023-02-02 17:52:23 -0300

    Refactored for into while loop for counter to not reset to 3

--- 7-days-of-code/day4/day4.js
+++ 7-days-of-code/day4/day4.js
@@ -1,33 +1,35 @@
 const number = 5
 
-// Math.floor(Math.random() * 10) + 1
-
 function validateUserNumber(number) {
   return !isNaN(Number(number)) && number >= 1 && number <= 10
 }
 
 function handleInput(number) {
   let userNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
-  if (!validateUserNumber(userNumber)) {
-    alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
-    return handleInput(number)
-  }
+  let counter = 3
+  let inputInvalid = false
 
-  userNumber = Number(userNumber)
-  for (let counter = 3; counter >= 0; counter--) {
-    if (userNumber === number) {
-      alert(`That's right, The number ${userNumber} is correct!`)
-      break
-    } else if (counter === 0) {
-      alert(`You're done, FINISHED! The answer was ${number}. Move to the mountains and disappear.`)
+  while (counter >= 0) {
+    if (!validateUserNumber(userNumber)) {
+      alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
+      inputInvalid = true
+      userNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
     } else {
-      alert(`WRONG! God have mercy on your soul. You have ${counter} more attempts`)
-      let newUserNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
-      if (!validateUserNumber(newUserNumber)) {
-        alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
-        return handleInput(number)
+      userNumber = Number(userNumber)
+      if (userNumber === number) {
+        alert(`That's right, The number ${userNumber} is correct!`)
+        break
+      } else if (counter === 0) {
+        alert(`You're done, FINISHED! The answer was ${number}. Move to the mountains and disappear.`)
+        break
+      } else {
+        alert(`WRONG! God have mercy on your soul. You have ${counter} more attempts`)
+        inputInvalid = false
+        userNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
       }
-      userNumber = Number(newUserNumber)
+    }
+    if (!inputInvalid) {
+      counter--
     }
   }
 }
