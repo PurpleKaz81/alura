e513182297dc43377c39a6f22486a3d1ea019906
Rafael Kasinski <rakasin@gmail.com>
2023-02-02 17:20:37 -0300

    Almost done. Still have to make sure counter doesn't ever reset

--- 7-days-of-code/day4/day4.js
+++ 7-days-of-code/day4/day4.js
@@ -1,21 +1,37 @@
-const number = Math.floor(Math.random() * 10) + 1
+const number = 5
+
+// Math.floor(Math.random() * 10) + 1
 
 function validateUserNumber(number) {
-  return !isNaN(Number(number)) && number <= 1 && number >= 10
+  return !isNaN(Number(number)) && number >= 1 && number <= 10
 }
 
 function handleInput(number) {
-  let userNumber
+  let userNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
   if (!validateUserNumber(userNumber)) {
-    userNumber = window.prompt(`Hi there! Choose a number between 1 and 10. Integers only`)
-    if (!validateUserNumber(userNumber)) {
-      alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
-      return handleInput()
+    alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
+    return handleInput(number)
+  }
+
+  userNumber = Number(userNumber)
+  for (let counter = 3; counter >= 0; counter--) {
+    if (userNumber === number) {
+      alert(`That's right, The number ${userNumber} is correct!`)
+      break
+    } else if (counter === 0) {
+      alert(`You're done, FINISHED! The answer was ${number}. Move to the mountains and disappear.`)
+    } else {
+      alert(`WRONG! God have mercy on your soul. You have ${counter} more attempts`)
+      let newUserNumber = window.prompt(`Hi there! Guess a whole number between 1 and 10. Let's see if you're right`)
+      if (!validateUserNumber(newUserNumber)) {
+        alert("Please type in any whole number between 1 and 10 using only numbers. No decimals or fractions, please.")
+        return handleInput(number)
+      }
+      userNumber = Number(newUserNumber)
     }
-    alert(`Okay, then. ${userNumber} it is!`)
   }
 }
 
 window.onload = () => {
-  handleInput()
+  handleInput(number)
 }
