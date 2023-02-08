58c1aeaaf9c08afd37c9fcff705a3eeaf4fb720e
Rafael Kasinski <rakasin@gmail.com>
2023-02-08 12:11:40 -0300

    Started with removal of groceries

--- 7-days-of-code/day6/day6.js
+++ 7-days-of-code/day6/day6.js
@@ -31,7 +31,23 @@ function validateChoice1(choice1) {
     case "1":
     case "2":
       return true
+
       break
+
+    default:
+      alert("Please input either '1' or '2'.")
+      return false
+  }
+}
+
+function validateChoice2(choice2) {
+  switch (choice2) {
+    case "1":
+    case "2":
+      return true
+
+      break
+
     default:
       alert("Please input either '1' or '2'.")
       return false
@@ -66,6 +82,7 @@ let counter = 0
 function addGrocery(groceryItem) {
   counter++
   if (counter > 10) {
+    counter = 0
     return
   }
   let choice1 = getUserInput("Would you like to shop today? Click '1' for yes and '2' for no.")
@@ -87,22 +104,58 @@ function addGrocery(groceryItem) {
   }
 }
 
-function displayCategories() {
-  const ul = document.querySelector("#grocery-list");
-  ul.style.listStyleType = "none";
-  ul.innerHTML = "";
+function removeGrocery(groceryItem) {
+  counter++
+  if (counter > 10) {
+    counter = 0
+    return
+  }
+  let choice2 = getUserInput("Would you like to remove any items today? Click '1' for yes and '2' for no.")
+  if (!validateChoice2(choice2)) {
+    alert("Please input either '1' or '2'.")
+    return removeGrocery()
+  } else {
+    let negativeGrocery = getUserInput("Which item will you remove? Type in the item's name.")
+    while (!validateGrocery(negativeGrocery)) {
+      alert("Please type in a valid name for an item")
+      negativeGrocery = getUserInput("So what item will you remove? Type in one at a time.")
+    }
+  }
+  let foundCategory = Object.keys(categories).find(key => categories[key].includes(capitalizeGrocery(negativeGrocery)))
 
-  Object.entries(categories)
-    .filter(([, items]) => items.length > 0)
-    .forEach(([category, items]) => {
-      const li = document.createElement("li");
-      li.textContent = `${category}: ${items.join(", ")}`;
-      ul.appendChild(li);
-    });
+  if (!foundCategory) {
+    alert("No such item was found in the list")
+  } else {
+    let index = categories[foundCategory].indexOf(capitalizeGrocery(negativeGrocery))
+    categories[foundCategory].splice(index, 1)
+
+    if (category[foundCategory].length === 0) {
+      delete categories[foundCategory]
+    }
+  }
 }
 
 function displayList() {
-  displayCategories()
+  let list = document.querySelector("#grocery-list")
+  list.style.listStyleType = "none"
+  list.innerHTML = ""
+
+  for (const category of Object.keys(categories)) {
+    let items = categories[category]
+    if (items.length > 0) {
+      let categoryElement = document.createElement("li")
+      categoryElement.innerHTML = `<b>${category}:</b>`
+      list.appendChild(categoryElement)
+      let itemsList = document.createElement("ul")
+      itemsList.style.listStyleType = "decimal"
+      for (const item of items) {
+        let itemElement = document.createElement("li")
+        itemElement.innerHTML = item
+        itemsList.appendChild(itemElement)
+      }
+      list.appendChild(itemsList)
+    }
+  }
 }
 
 window.onload = () => {
