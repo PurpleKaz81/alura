df6dc4741ae3ea8ecdf65be2a6e5f134596b0e1c
Rafael Kasinski <rakasin@gmail.com>
2023-03-03 16:02:42 -0300

    Massively refactored, I'm all giddy

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,3 +1,5 @@
+const { ENTER, ESC } = { ENTER: "Enter", ESC: "Escape" }
+
 const sayHi = () => {
   const message = "Hi, bitch! Tell yo mama I'm home."
   console.log(message)
@@ -10,7 +12,21 @@ const sayHi2 = () => {
   alert(message2)
 }
 
-const = 
+const robotron = document.querySelector("#producao")
+
+const handleEvent = (event) => {
+  if (event.type === "click") {
+    event.preventDefault()
+    sayHi2()
+  } else if (event.type === "keydown") {
+    if (event.key === ENTER || event.key === ESC) {
+      alert.close()
+    }
+  }
+}
+
+const eventTypes = ["click", "keydown"]
+
+eventTypes.forEach((type) => robotron.addEventListener(type, handleEvent))
 
 document.addEventListener("DOMContentLoaded", sayHi)
-document.querySelector("#producao").addEventListener("click", sayHi2)
