9bef40f20398cc8c5d1e102eb4bc79bad35d4c1f
Rafael Kasinski <rakasin@gmail.com>
2023-03-03 18:08:32 -0300

    Made incriments more universal in JS code

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,56 +1,51 @@
-const { ENTER, ESC } = { ENTER: "Enter", ESC: "Escape" }
-const eventTypes = ["click", "keydown"]
+document.addEventListener("DOMContentLoaded", () => {
+  const { ENTER, ESC } = { ENTER: "Enter", ESC: "Escape" }
+  const eventTypes = ["click", "keydown"]
+  const producao = document.querySelector("#producao")
+  const countButtons = document.querySelectorAll(".controle-ajuste")
 
-const [robotron, somarBraco, subtrairBraco, braco] = [
-  "#producao",
-  "#somar-braco",
-  "#subtrair-braco",
-  "#braco"
-].map((id) => document.querySelector(id))
-
-const sayHi = () => {
-  const message = "Tell yo mama I'm home."
-  alert(message)
-}
-
-const sayHi2 = () => {
-  const message2 = "Whaaaaaat?!"
-  alert(message2)
-}
-
-const changeButtonValue = () => {
-  const smirk = "\uD83D\uDE0F"
-  robotron.classList.add("override")
-  robotron.value = `You touched me... ${smirk}`
-    .toLowerCase()
-    .replace(/^\w/, (s) => s.toUpperCase())
-}
-
-const handleEvent = (event) => {
-  switch (event.type) {
-    case "click":
-      event.preventDefault()
-      changeButtonValue()
-      sayHi2()
-      break
-    case "keydown":
-      if (event.key === ENTER || event.key === ESC) {
-        alert.close()
-      }
-      break
+  const sayHi = () => {
+    const message = "Tell yo mama I'm home."
+    alert(message)
   }
-}
 
-eventTypes.forEach((type) => robotron.addEventListener(type, handleEvent))
+  const sayHi2 = () => {
+    const message2 = "Whaaaaaat?!"
+    alert(message2)
+  }
 
-somarBraco.addEventListener("click", (event) => {
-  event.preventDefault()
-  braco.value = parseInt(braco.value) + 1
+  const changeButtonValue = () => {
+    const smirk = "\uD83D\uDE0F"
+    producao.classList.add("override")
+    producao.value = `You touched me... ${smirk}`
+      .toLowerCase()
+      .replace(/^\w/, (s) => s.toUpperCase())
+  }
+
+  const handleEvent = (event) => {
+    switch (event.type) {
+      case "click":
+        event.preventDefault()
+        changeButtonValue()
+        sayHi2()
+        break
+      case "keydown":
+        if (event.key === ENTER || event.key === ESC) {
+          alert.close()
+        }
+        break
+    }
+  }
+
+  eventTypes.forEach((type) => producao.addEventListener(type, handleEvent))
+
+  countButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault()
+      const braco = document.querySelector("#braco")
+      const increment = button.id === "somar" ? 1 : 1
+      braco.value = parseInt(braco.value) + increment
+    })
+  })
+  sayHi()
 })
-
-subtrairBraco.addEventListener("click", (event) => {
-  event.preventDefault()
-  braco.value = parseInt(braco.value) - 1
-})
-
-document.addEventListener("DOMContentLoaded", sayHi)
