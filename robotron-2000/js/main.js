7ffac4c23e08917c28d1fc843263d52646c09931
Rafael Kasinski <rakasin@gmail.com>
2023-03-03 15:08:39 -0300

    Inserted some basic alerts and logs

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,3 +1,16 @@
-window.addEventListener("DOMContentLoaded", () => {
-  alert("Tell yo mama I'm here")
-})
+const sayHi = () => {
+  const message = "Hi, bitch! Tell yo mama I'm home."
+  console.log(message)
+  alert(message)
+}
+
+const sayHi2 = () => {
+  const message2 = "Whaaaaaat?!"
+  console.log(message2)
+  alert(message2)
+}
+
+const = 
+
+document.addEventListener("DOMContentLoaded", sayHi)
+document.querySelector("#producao").addEventListener("click", sayHi2)
