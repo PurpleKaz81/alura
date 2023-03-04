95cdfdbb47c949f2057b777093ec36016993d1dc
Rafael Kasinski <rakasin@gmail.com>
2023-03-04 16:11:01 -0300

    Refactoring, old method too complex

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -1,53 +1,63 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const { ENTER, ESC } = { ENTER: "Enter", ESC: "Escape" }
-  const eventTypes = ["click", "keydown"]
-  const producao = document.querySelector("#producao")
-  const countButtons = document.querySelectorAll(".controle-ajuste")
+const { ENTER, ESC } = { ENTER: "Enter", ESC: "Escape" }
+const eventTypes = ["click", "keydown"]
+const producao = document.querySelector("#producao")
 
-  const sayHi = () => {
-    const message = "Tell yo mama I'm home."
-    alert(message)
-  }
+const subtrair = document.querySelector("#subtrair-braco")
+const somar = document.querySelector("#somar-braco")
+const braco = document.querySelector("#braco")
 
-  const sayHi2 = () => {
-    const message2 = "Whaaaaaat?!"
-    alert(message2)
-  }
+const sayHi = () => {
+  const message = "Tell yo mama I'm home."
+  alert(message)
+}
 
-  const changeButtonValue = () => {
-    const smirk = "\uD83D\uDE0F"
-    producao.classList.add("override")
-    producao.value = `You touched me... ${smirk}`
-      .toLowerCase()
-      .replace(/^\w/, (s) => s.toUpperCase())
-  }
+const sayHi2 = () => {
+  const message2 = "Whaaaaaat?!"
+  alert(message2)
+}
 
-  const handleEvent = (event) => {
-    switch (event.type) {
-      case "click":
-        event.preventDefault()
-        changeButtonValue()
-        sayHi2()
-        break
-      case "keydown":
-        if (event.key === ENTER || event.key === ESC) {
-          alert.close()
-        }
-        break
-    }
-  }
+const changeButtonValue = () => {
+  const smirk = "\uD83D\uDE0F"
+  producao.classList.add("override")
+  producao.value = `You touched me... ${smirk}`
+    .toLowerCase()
+    .replace(/^\w/, (s) => s.toUpperCase())
+}
 
-  eventTypes.forEach((type) => producao.addEventListener(type, handleEvent))
-
-  countButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
+const handleAlertEvent = (event) => {
+  switch (event.type) {
+    case "click":
       event.preventDefault()
-      console.log(button.id.split("-"))
-      const [action, target] = button.id.split("-")
-      const input = document.querySelector(`#${target}`)
-      const increment = action === "somar" ? 1 : -1
-      input.value = parseInt(input.value) + increment
-    })
+      changeButtonValue()
+      sayHi2()
+      break
+    case "keydown":
+      if (event.key === ENTER || event.key === ESC) {
+        alert.close()
+      }
+      break
+  }
+}
+
+const manipulateCount = (operation) => {
+  if (operation === subtrair) {
+    braco.value = parseInt(braco.value) - 1
+  } else {
+    braco.value = parseInt(braco.value) + 1
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  eventTypes.forEach((type) => producao.addEventListener(type, handleAlertEvent))
+
+  somar.addEventListener("click", (event) => {
+    event.preventDefault()
+    manipulateCount(somar)
+  })
+
+  subtrair.addEventListener("click", (event) => {
+    event.preventDefault()
+    manipulateCount(subtrair)
   })
 
   sayHi()
