4c365df2df2432de9b752211b73b505f10bf8e1f
rafael kasinski <rakasin@gmail.com>
2023-03-08 16:26:07 -0300

    Server code done, with client table being filled in JS

--- async-javascript/service/client-service.js
+++ async-javascript/service/client-service.js
@@ -1,3 +1,22 @@
+const createNewLine = (nome, email) => {
+  const newClientLine = document.createElement("tr")
+  const content = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+      <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+      </ul>
+    </td>
+    `
+
+  newClientLine.innerHTML = content
+  return newClientLine
+}
+
+const table = document.querySelector("[data-tabela]")
+
 const http = new XMLHttpRequest()
 
 http.open("GET", "http://localhost:3000/profile")
@@ -5,6 +24,8 @@ http.open("GET", "http://localhost:3000/profile")
 http.send()
 
 http.onload = () => {
-  const data = http.response
-  console.log(data)
+  const data = JSON.parse(http.response)
+  data.forEach((element) => {
+    table.appendChild(createNewLine(element.nome, element.email))
+  })
 }
