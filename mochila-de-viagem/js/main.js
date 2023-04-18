1803778e6a52c26aeae092dd26fca9f87508b985
rafael kasinski <rakasin@gmail.com>
2023-04-18 17:54:35 -0300

    Finished adding new item with quantity

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,7 +1,24 @@
 const form = document.querySelector("#novoItem")
+const list = document.querySelector("#lista")
+
+const createElement = (name, quantity) => {
+  // replicate <li class="item"><strong>7</strong>Camisas</li>
+  const newItem = document.createElement("li")
+  newItem.classList.add("item")
+
+  const itemQuantity = document.createElement("strong")
+  itemQuantity.innerHTML = quantity
+
+  newItem.appendChild(itemQuantity)
+  newItem.innerHTML += name
+
+  list.appendChild(newItem)
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
-  console.log(event.target.elements['nome'].value)
-  console.log(event.target.elements['quantidade'].value)
+  const name = event.target.elements['nome'].value
+  const quantity = event.target.elements['quantidade'].value
+
+  createElement(name, quantity)
 })
