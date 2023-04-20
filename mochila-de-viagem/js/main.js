cf863483ab035cda707f892fa37fd5704e88f99f
rafael kasinski <rakasin@gmail.com>
2023-04-20 11:27:07 -0300

    Finished 4/5

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -9,11 +9,16 @@ const createElement = (item) => {
 
   const itemQuantity = document.createElement("strong")
   itemQuantity.innerHTML = item.quantity
+  itemQuantity.dataset.id = item.id
 
   newItem.appendChild(itemQuantity)
   newItem.innerHTML += item.name
 
   list.appendChild(newItem)
+}
+
+const updateElement = (item) => {
+  document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantity
 }
 
 
@@ -26,14 +31,23 @@ form.addEventListener("submit", (event) => {
   const name = event.target.elements['nome']
   const quantity = event.target.elements['quantidade']
 
+  const exists = items.find( element => element.name === name.value)
+
   const presentItem = {
     "name": name.value,
     "quantity": quantity.value
   }
 
-  createElement(presentItem)
+  if (exists) {
+    presentItem.id = exists.id
+    updateElement(presentItem)
+    items[exists.id] = presentItem
+  } else {
+    presentItem.id = items.length
+    createElement(presentItem)
+    items.push(presentItem)
+  }
 
-  items.push(presentItem)
 
   localStorage.setItem("items", JSON.stringify(items))
 
