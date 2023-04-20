6ce070d711d1eadded040a89047aca9f09733f33
rafael kasinski <rakasin@gmail.com>
2023-04-20 10:58:40 -0300

    Finished 3/5

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,37 +1,42 @@
 const form = document.querySelector("#novoItem")
 const list = document.querySelector("#lista")
-const items = []
+const items = JSON.parse(localStorage.getItem("items")) || []
+
+const createElement = (item) => {
+  // replicate <li class="item"><strong>7</strong>Camisas</li>
+  const newItem = document.createElement("li")
+  newItem.classList.add("item")
+
+  const itemQuantity = document.createElement("strong")
+  itemQuantity.innerHTML = item.quantity
+
+  newItem.appendChild(itemQuantity)
+  newItem.innerHTML += item.name
+
+  list.appendChild(newItem)
+}
+
+
+items.forEach((item) => {
+  createElement(item)
+})
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
   const name = event.target.elements['nome']
   const quantity = event.target.elements['quantidade']
 
-  createElement(name.value, quantity.value)
+  const presentItem = {
+    "name": name.value,
+    "quantity": quantity.value
+  }
+
+  createElement(presentItem)
+
+  items.push(presentItem)
+
+  localStorage.setItem("items", JSON.stringify(items))
 
   name.value = ""
   quantity.value = ""
 })
-
-const createElement = (name, quantity) => {
-  // replicate <li class="item"><strong>7</strong>Camisas</li>
-  const newItem = document.createElement("li")
-  newItem.classList.add("item")
-
-  const itemQuantity = document.createElement("strong")
-  itemQuantity.innerHTML = quantity
-
-  newItem.appendChild(itemQuantity)
-  newItem.innerHTML += name
-
-  list.appendChild(newItem)
-
-  const presentItem = {
-    "name": name,
-    "quantity": quantity
-  }
-
-  items.push(presentItem)
-
-  localStorage.setItem("item", JSON.stringify(items))
-}
