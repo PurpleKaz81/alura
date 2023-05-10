a3b86998141a4f521c7e1af32df224c6ac0396ab
rafael kasinski <rakasin@gmail.com>
2023-05-10 13:04:16 -0300

    Added while loops and fixed formatting for terminal

--- python-comecando/guessing-game/guessing-2.py
+++ python-comecando/guessing-game/guessing-2.py
@@ -2,55 +2,78 @@
 
 import random
 
-print("***" * 10, "\n")
-print("Guess the number between 1 and 10")
+while True:
+    print("***" * 10, "\n")
+    print("Welcome to Guess The Number! Wanna play?", "\n")
 
-tries_left = 3
-tries_taken = 0
-guessed_numbers = []
-while tries_left > 0:
-    guess = input("Type in your guess: ")
-    try:
-        guess = int(guess)
-        if guess < 1 or guess > 10:
-            print("Only whole numbers between 1 and 10, champ.")
-        elif guess in guessed_numbers:
-            print("You already guessed that number. Try again.")
+    while True:
+        answer = input("Type [y] for yes and [n] for no.\n\n")
+        print()
+
+        if answer == "n":
+            print("Goodbye, then...", "\U0001F984", "\n")
+            break
+        elif answer == "y":
+            break
         else:
-            print("You typed:", guess)
+            print("Invalid input.", "\n")
 
-            correct_number = random.randint(1, 10)
-            if guess == correct_number:
-                print("\U0001F3AF " * 3, "Nailed it!", "\U0001F3AF " * 3)
-                break
-            elif guess > correct_number:
-                print("\u2B07\ufe0f  " * 3, "Hmmm, a little smaller...", "\u2B07\ufe0f  " * 3)
-                tries_left -= 1
-                tries_taken += 1
-            else:
-                print("\u2B06\ufe0f  " * 3, "A little higher, love...", "\u2B06\ufe0f  " * 3)
-                tries_left -= 1
-                tries_taken += 1
-                print(
-                    "You have",
-                    tries_left,
-                    "tries left.",
-                )
+    print("***" * 10, "\n")
+    print("Guess the number between 1 and 10", "\n")
 
-            guessed_numbers.append(guess)
-    except ValueError:
+    tries_left = 3
+    tries_taken = 0
+    guessed_numbers = []
+    correct_number = random.randint(1, 10)
+
+    while tries_left > 0:
+        guess = input("\nType in your guess: \n\n")
         try:
-            float(guess)
-            print("Type in a whole number, please.")
+            guess = int(guess)
+            if guess < 1 or guess > 10:
+                print("\nOnly whole numbers between 1 and 10, champ.", "\n")
+            elif guess in guessed_numbers:
+                print("\nYou already guessed that number. Try again.", "\n")
+            else:
+                print("\nYou typed:", guess, "\n")
 
+                if guess == correct_number:
+                    print("\U0001F3AF " * 3, "Nailed it!", "\U0001F3AF " * 3, "\n")
+                    break
+                elif guess > correct_number:
+                    print("\u2B07\ufe0f  " * 3, "Hmmm, a little smaller...", "\u2B07\ufe0f  " * 3, "\n")
+                    tries_left -= 1
+                    tries_taken += 1
+                else:
+                    print("\u2B06\ufe0f  " * 3, "A little higher, love...", "\u2B06\ufe0f  " * 3, "\n")
+                    tries_left -= 1
+                    tries_taken += 1
+                    print("You have", tries_left, "tries left.", "\n")
+
+                guessed_numbers.append(guess)
         except ValueError:
-            print("Type in a number, buddy.")
+            try:
+                float(guess)
+                print("Type in a whole number, please.", "\n")
 
-print("Game O-VER!")
-if tries_left == 0:
-    print("You lose. There is meaning in nothing \U0001F603")
-else:
-    tries_taken += 1
-    print("Congrats! You won in", tries_taken, "tries.")
+            except ValueError:
+                print("Type in a number, buddy.", "\n")
 
-print("\n", "***" * 10, "\n")
+    print("Game O-VER!", "\n")
+    if tries_left == 0:
+        print("You lose. There is meaning in nothing \U0001F603", "\n")
+    else:
+        tries_taken += 1
+        print("Congrats! You won in", tries_taken, "tries.", "\n")
+
+    while True:
+        answer = input("Wanna play again, pumpkin? Type [y] for yes and [n] for no.\n\n")
+        if answer == "n":
+            print("Goodbye, then...", "\U0001F984", "\n")
+            break
+        elif answer == "y":
+            break
+        else:
+            print("Do us a solid an type in [y] or [n].\n")
+
+    print("\n", "***" * 10, "\n")
