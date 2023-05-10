9a2157d51324cd8381dd2ae3d814868b07495742
rafael kasinski <rakasin@gmail.com>
2023-05-10 14:26:38 -0300

    Slight change to formatting and in conditional

--- python-comecando/guessing-game/guessing-2.py
+++ python-comecando/guessing-game/guessing-2.py
@@ -4,9 +4,11 @@ import random
 
 already_played = False
 
+print()
+print("***" * 10, "\n")
+
 while True:
     if not already_played:
-        print("***" * 10, "\n")
         print("Welcome to Guess The Number! Wanna play?", "\n")
 
         while True:
@@ -38,6 +40,7 @@ while True:
             guess = int(guess)
             acertado = guess == correct_number
             maior = guess > correct_number
+            menor = guess < correct_number
             if guess < 1 or guess > 10:
                 print("\nOnly whole numbers between 1 and 10, champ.", "\n")
             elif guess in guessed_numbers:
@@ -48,17 +51,18 @@ while True:
                 if acertado:
                     print("\U0001F3AF " * 3, "Nailed it!", "\U0001F3AF " * 3, "\n")
                     break
-                elif maior:
-                    print("\u2B07\ufe0f  " * 3, "Hmmm, a lower lower...", "\u2B07\ufe0f  " * 3, "\n")
-                    tries_left -= 1
-                    tries_taken += 1
                 else:
-                    print("\u2B06\ufe0f  " * 3, "A little higher, love...", "\u2B06\ufe0f  " * 3, "\n")
-                    tries_left -= 1
-                    tries_taken += 1
-                    print("You have", tries_left, "tries left.", "\n")
+                    if maior:
+                        print("\u2B07\ufe0f  " * 3, "Hmmm, a lower lower...", "\u2B07\ufe0f  " * 3, "\n")
+                        tries_left -= 1
+                        tries_taken += 1
+                    elif menor:
+                        print("\u2B06\ufe0f  " * 3, "A little higher, love...", "\u2B06\ufe0f  " * 3, "\n")
+                        tries_left -= 1
+                        tries_taken += 1
+                        print("You have", tries_left, "tries left.", "\n")
 
-                guessed_numbers.append(guess)
+                        guessed_numbers.append(guess)
         except ValueError:
             try:
                 float(guess)
