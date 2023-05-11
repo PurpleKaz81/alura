3bded7dbbd7f984c135adcc5ac220202d9e7267e
rafael kasinski <rakasin@gmail.com>
2023-05-11 13:43:05 -0300

    Finished testing strings and 05/09

--- python-comecando/string-formatting.py
+++ python-comecando/string-formatting.py
@@ -19,3 +19,17 @@ print("R${:7d}".format(46))
 print("Date {:02d}/{:02d}".format(9, 4))
 
 print("Date {:02d}/{:02d}".format(19, 11))
+
+name = "Leonardo"
+last_name = "Cordeiro"
+sign = "peixes"
+condition = "homão da porra"
+
+print("Olá, Sr. {1}, {0}, de {2}, {3}".format(name, last_name, sign, condition))
+
+name = "Rodrigo"
+last_name = "Mattos"
+sign = "câncer"
+condition = "delícia viva e Sazon\u00ae dos meus olhos \U0001F60D"
+
+print(f"Olá, Sr. {last_name}, {name}, de {sign}, {condition} ")
