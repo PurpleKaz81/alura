d222be80073167caf7ee19534c7b7eb1050b07be
rafael kasinski <rakasin@gmail.com>
2023-05-12 12:37:02 -0300

    Refactored and formatted. USed dictionary for games and employed more functions. Finished 8/9

--- python-comecando/games.py
+++ python-comecando/games.py
@@ -4,46 +4,61 @@ import hangman.hangman as hangman
 import guessing_game.guessing2 as guessing2
 
 
+def print_menu():
+    print("*************************************************************")
+    print("********************** Choose Your Game! ********************")
+    print("*************************************************************", "\n")
+
+    print(
+        "Please type the number corresponding to the game you'd like to play:\n",
+        "- [1] Hangman",
+        "- [2] Guess the Number",
+        "- [3] Quit.",
+        sep="\n",
+        end="\n\n",
+    )
+
+
+def question_option(already_played):
+    if already_played:
+        game_choice = int(input("What game would you like to play?\n\n"))
+    else:
+        game_choice = int(input("Which one will it be?\n\n"))
+        already_played = True
+    return game_choice, already_played
+
+
+def invalid_choice():
+    print("Oops! That's not a valid choice. Please choose 1, 2, or 3.", "\n")
+
+
+def goodbye():
+    print("\nGoodbye, then...", "\U0001F984", "\n")
+    print("***" * 10, "\n")
+
+
 def pick_game():
     already_played = False
+    games = {1: hangman.play, 2: guessing2.play}
 
     while True:
         try:
-            print("*************************************************************")
-            print("********************** Choose Your Game! ********************")
-            print("*************************************************************", "\n")
+            print_menu()
 
-            print(
-                "Please type the number corresponding to the game you'd like to play:\n",
-                "- [1] Hangman",
-                "- [2] Guess the Number",
-                "- [3] Quit.",
-                sep="\n",
-                end="\n\n",
-            )
+            game_choice, already_played = question_option(already_played)
 
-            if already_played:
-                game_choice = int(input("What game would you like to play?\n\n"))
-            else:
-                game_choice = int(input("Which one will it be?\n\n"))
-                already_played = True
-
-            if game_choice == 1:
-                hangman.play()
-            elif game_choice == 2:
-                guessing2.play()
+            if game_choice in games:
+                games[game_choice]()
             elif game_choice == 3:
-                print("\nGoodbye, then...", "\U0001F984", "\n")
-                print("***" * 10, "\n")
+                goodbye()
                 break
             else:
-                print(
-                    "Oops! That's not a valid choice. Please choose 1, 2, or 3.", "\n"
-                )
+                invalid_choice()
         except ValueError:
-            print("Oops! That's not a valid choice. Please choose 1, 2, or 3.", "\n")
+            invalid_choice
 
 
 if __name__ == "__main__":
-    print("***" * 10, "This is the main menu!", "***" * 10, "\n")
+    print()
+    print("This is the main menu!", "\n")
     pick_game()
