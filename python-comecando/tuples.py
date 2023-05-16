51d204691702dde0c0e8a0613ba2353e4f137b82
rafael kasinski <rakasin@gmail.com>
2023-05-16 16:25:17 -0300

    Worked on tuples

--- python-comecando/tuples.py
+++ python-comecando/tuples.py
@@ -24,3 +24,34 @@ days.remove("Friday")
 days[-2:] = [", and ".join(days[-2:])]
 
 print(f"The days of the week are {', '.join(days)}.")
+
+# three points on a graph to graph a right triangle
+point1 = (0, 0)
+point2 = (3, 0)
+point3 = (0, 4)
+triangle = [point1, point2, point3]
+
+# command to draw this triangle in terminal with print(), lines included
+print("\n".join(["".join(["*" if (x, y) in triangle else " " for x in range(4)]) for y in range(5)]))
+
+employee1 = ("Rafael", 41)
+employee2 = ("Anne", 28)
+employee3 = ("Jen", 32)
+
+employees = [employee1, employee2, employee3]
+print(employees)
+print(f"{employees[0][0]} is {employees[0][1]} years old, {employees[0][1] - employees[1][1]} years older than {employees[1][0]}, who is {employees[1][1]}.")
+
+total_age = sum(employee[1] for employee in employees)
+print(f"The employees' total age is {total_age}.")
+
+employee4 = ("Satan", 666)
+employees_copy = employees.copy()
+
+employees_copy.append(employee4)
+
+print(f"These four employees will work here forever: {', '.join(employee[0] for employee in employees_copy[:-1])}, and {employees_copy[-1][0]}.")
+
+employees_copy_tuple = tuple(employees_copy)
+
+print(tuple(employees_copy_tuple))
