5c6a591498823df065508d0383cdaaec7ca93a7e
rafael kasinski <rakasin@gmail.com>
2023-05-16 13:16:16 -0300

    Still repeating first question, but everything else in place. Finished 3/5

--- python-comecando/hangman/hangman.py
+++ python-comecando/hangman/hangman.py
@@ -2,13 +2,13 @@
 
 
 def goodbye():
-    print("\nGoodbye, then...", "\U0001F984", "\n")
+    print("Goodbye, then...", "\U0001F984", "\n")
     print("***" * 10, "\n")
 
 
 def want_to_play():
     while True:
-        answer = input("Type [y] for yes and [n] for no.\n\n")
+        answer = input("Wanna play? Type [y] for yes and [n] for no.\n\n")
         print()
 
         if answer == "n":
@@ -20,17 +20,33 @@ def want_to_play():
             print("Just y or n, buddy.", "\n")
             return False
 
+def wanna_play_again():
+    while True:
+        answer = input("Wanna play again? Type [y] for yes and [n] for no.\n\n")
+        print()
+
+        if answer == "n":
+            goodbye()
+            return False
+        elif answer == "y":
+            return True
+        else:
+            print("Just y or n, buddy.", "\n")
+            return False
 
 def play():
     already_played = False
-    hanged = False
-    success = False
-    secret_word = "banana"
+
 
     print()
     print("***" * 10, "\n")
 
+
     while True:
+        hanged = False
+        success = False
+        secret_word = "banana"
+        correct_guesses = ["_", "_", "_", "_", "_", "_"]
         if not already_played:
             print("Welcome to Hangman!", "\n")
 
@@ -39,11 +55,28 @@ def play():
 
         while not hanged and not success:
             guess = input("Guess a letter: ").strip().lower()
+            found = False
 
             for index, letter in enumerate(secret_word):
                 if guess == letter:
-                    print(f"Found {guess} at position {index}")
+                    correct_guesses[index] = letter
+                    found = True
 
+            print()
+            print(correct_guesses, "\n")
+
+            if not found:
+                print(f"{guess} not found in word.", "\n")
+
+            if "_" not in correct_guesses:
+                success = True
+                print("\U0001F973 " * 3, "You won!", "\U0001F973 " * 3, "\n")
+
+        if not already_played:
+            already_played = True
+
+        if not wanna_play_again():
+            break
 
 print("Game Over!", "\n")
 
