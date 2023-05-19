d0382c3e48f17bba04449407756661b82dc06559
rafael kasinski <rakasin@gmail.com>
2023-05-19 14:09:37 -0300

    New methods for error handling

--- python-comecando/hangman/hangman.py
+++ python-comecando/hangman/hangman.py
@@ -27,8 +27,8 @@ def print_failure_message(secret_word):
 
 
 def wrong_guess_messages(errors, guess):
-    print(f"\nThat's incorrect! You have {6 - errors} tries left.")
-    print(f"\nThe word does not contain {guess}.", "\n")
+    print(f"\nThat's incorrect! The word does not contain {guess}.")
+    print(f"\nYou have {6 - errors} tries left.", "\n")
 
 
 def get_user_confirmation_prompt(prompt):
@@ -74,12 +74,49 @@ def get_valid_guess(guessed_letters):
             return guess
 
 
+def guess_is_letter(guess, secret_word, correct_guesses):
+    found = False
+    for index, letter in enumerate(secret_word):
+        if guess == letter:
+            correct_guesses[index] = letter
+            found = True
+    return found
+
+
+def correct_guess(guess, secret_word, correct_guesses):
+    if found := guess_is_letter(guess, secret_word, correct_guesses):
+        if correct_guesses.count("_") > 0:
+            print()
+            print("".join(correct_guesses), "\n")
+        elif correct_guesses.count("_") == 0:
+            success = True
+            print()
+            print_success_message(secret_word)
+            return True
+    return False
+
+
+def error_or_failure(errors, guess, hanged, secret_word, game_on):
+    hanged = False
+    game_on = True
+    errors += 1
+    if errors < 6:
+        wrong_guess_messages(errors, guess)
+    elif errors == 6:
+        hanged = True
+        print()
+        print_failure_message(secret_word)
+        game_on = False
+    return hanged, game_on, errors
+
 def play():
     already_played = False
     print("***" * 10, "\n")
 
     secret_word = select_secret_word()
     print(secret_word)
+
+    found = False
 
     while True:
         game_on, already_played = start_or_continue_game(already_played)
@@ -91,34 +128,14 @@ def play():
         guessed_letters = []
 
         while game_on and not hanged and not success:
-            found = False
-
             guess = get_valid_guess(guessed_letters)
 
             guessed_letters.append(guess)
 
             if guess in secret_word:
-                for index, letter in enumerate(secret_word):
-                    if guess == letter:
-                        correct_guesses[index] = letter
-                        found = True
-                if correct_guesses.count("_") > 0:
-                    print()
-                    print("".join(correct_guesses), "\n")
-                elif correct_guesses.count("_") == 0:
-                    success = True
-                    print()
-                    print_success_message(secret_word)
+                success = correct_guess(guess, secret_word, correct_guesses)
             else:
-                errors += 1
-                if errors < 6:
-                    wrong_guess_messages(errors, guess)
-                else:
-                    hanged = True
-                    print()
-                    print_failure_message(secret_word)
-                    game_on = False
-                    break
+                hanged, game_on, errors = error_or_failure(errors, guess, hanged, secret_word, game_on)
 
             if game_on:
                 remaining_letters = correct_guesses.count("_")
