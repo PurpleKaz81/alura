b5d218f2991914548706a9a9a8a0b04f58351a58
rafael kasinski <rakasin@gmail.com>
2023-05-26 18:29:50 -0300

    Placed an index for account info

--- python-oop/account.py
+++ python-oop/account.py
@@ -1,11 +1,11 @@
 class Account:
+
     def __init__(self, number, holder, balance, limit=1000):
         # print(f"Building object... {self}")
         self.number = number
         self.holder = holder
         self.balance = balance
         self.limit = limit
-
 
     def format_value(self, value, account_number=None):
         if account_number is not None and account_number == value:
@@ -18,23 +18,23 @@ class Account:
         else:
             return "US${:,.2f}".format(value)
 
-
-
     def __str__(self):
         return f"Account: {self.number}, Holder: {self.holder}, Balance: {self.balance}, Limit: {self.limit}"
 
-
     def client_info(self, account):
         result = "Here's the client's information:\n"
-        for key, value in account.items():
-            result += f"{key}, {self.format_value(value, account['number'])}\n"
+        for index, (key, value) in enumerate(account.items(), start=1):
+            result += f"{index}) {key}: {self.format_value(value, account['number'])}\n"
         return result
+
+    def print_client_info(self):
+        print(self.client_info(vars(self)))
 
 
 account_1 = Account(123, "Nico", 1000)
 account_2 = Account(321, "Marco", 100)
 account_3 = Account(666, "Satan", 100, 2000)
 
-print(account_1.client_info(account_1.__dict__))
-print(account_2.client_info(account_2.__dict__))
-print(account_2.client_info(account_3.__dict__))
+account_1.print_client_info()
+account_2.print_client_info()
+account_3.print_client_info()
