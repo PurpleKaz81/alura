b2f12440a090099d011f8e4d5376f8e70dad7ac2
rafael kasinski <rakasin@gmail.com>
2023-05-29 11:39:00 -0300

    Formatted dates and imported that into account file

--- python-oop/account.py
+++ python-oop/account.py
@@ -1,3 +1,5 @@
+from dates import Date
+
 class Account:
 
     def __init__(self, number, holder, balance, limit=1000):
@@ -97,8 +99,10 @@ print(
 )
 print()
 account_3.deposit(1000000)
+
+date_1 = Date(12, 5, 2023)
 print(
-    f"{account_3.holder} just made {account_3.format_value(100000)} selling shit T-shirts, cuz life's unfair, so he now has {account_3.format_value(account_3.balance)}."
+    f"{account_3.holder} made {account_3.format_value(100000)} selling shit T-shirts on {date_1.formatted_date()}, cuz life's unfair, so he now has {account_3.format_value(account_3.balance)}."
 )
 account_2.deposit(85.67)
 print()
@@ -111,3 +115,6 @@ print()
 account_1.print_client_info()
 account_2.print_client_info()
 account_3.print_client_info()
+print()
+date_2 = Date(17, "08", 2017)
+print(f"On {date_2.formatted_date()}, {account_3.holder} came to visit and never really went back home.")
---
+++ python-oop/dates.py
@@ -0,0 +1,14 @@
+class Date:
+    def __init__(self, day, month, year):
+        self.day = int(day)
+        self.month = int(month)
+        self.year = int(year)
+
+    def formatted_date(self):
+        return f"{self.day:02d}/{self.month:02d}/{self.year:4d}"
+
+date_obj = Date(10, 9, 2023)
+print(date_obj.formatted_date(), "\n")
+
+date_obj_2 = Date(11, "03", 2020)
+print(date_obj_2.formatted_date())
