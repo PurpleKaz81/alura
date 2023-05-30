afb3f646e5d1607033d1f59601f64c05893cf78c
rafael kasinski <rakasin@gmail.com>
2023-05-30 12:39:20 -0300

    Corrected typo

--- python-oop/restaurants.py
+++ python-oop/restaurants.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
+
 class Restaurant:
+    _instances = []
 
     def __init__(self, name, cuisine_type, price_range, total_seats, open_time,
                  close_time, address, rating, delivery_service, menu, chef):
@@ -15,6 +17,8 @@ class Restaurant:
         self.delivery_service = delivery_service
         self.menu = menu
         self.chef = chef
+
+        self._instances.append(self)
 
     def _parse_open_close_time(self):  # sourcery skip: raise-specific-error
         try:
@@ -46,23 +50,23 @@ class Restaurant:
         except Exception as e:
             return str(e)
 
-
-def do_deliver(restaurants):
-    result = [
-        restaurant.name for restaurant in restaurants
-        if restaurant.delivery_service == True
-    ]
-    if len(result) > 2:
-        main_names = ", ".join(result[:-1])
-        last_name = result[-1]
-        return f"{main_names}, and {last_name}"
-    elif len(result) == 2:
-        return f"{result[0]} and {result[1]}"
-    elif len(result) == 1:
-        return f"{result[0]}"
-    else:
-        return "Guess no one delivers, huh?"
-    return "\n".join(result)
+    @classmethod
+    def do_deliver(cls):
+        result = [
+            restaurant.name for restaurant in cls._instances
+            if restaurant.delivery_service == True
+        ]
+        if len(result) > 2:
+            main_names = ", ".join(result[:-1])
+            last_name = result[-1]
+            return f"{main_names}, and {last_name}"
+        elif len(result) == 2:
+            return f"{result[0]} and {result[1]}"
+        elif len(result) == 1:
+            return f"{result[0]}"
+        else:
+            return "Guess no one delivers, huh?"
+        return "\n".join(result)
 
 
 def average_rating(restaurants):
@@ -162,71 +166,90 @@ def least_popular_cuisine_type(restaurants):
 
 
 def most_popular_menu_item(restaurants):
-    menu_items = [item for restaurant in restaurants for item in restaurant.menu]
+    menu_items = [
+        item for restaurant in restaurants for item in restaurant.menu
+    ]
     menu_items_count = Counter(menu_items)
     return menu_items_count.most_common(1)[0][0].lower()
 
 
-restaurant1 = Restaurant("Crustacean", "Vietnamese", "$$$", 50, "5PM", "10PM",
-                         "123 Main St", 4.5, True, ["Shrimp", "Noodle Soup"],
-                         ["Chef Nguyen", "Nguyen Van A", "5'6", "Culinary Institute of America"])
+restaurant1 = Restaurant(
+    "Crustacean", "Vietnamese", "$$$", 50, "5PM", "10PM", "123 Main St", 4.5,
+    True, ["Shrimp", "Noodle Soup"],
+    ["Chef Nguyen", "Nguyen Van A", "5'6", "Culinary Institute of America"])
 
-restaurant2 = Restaurant("Green Lotus", "Thai", "$", 40, "11AM", "11PM",
-                         "456 High St", 4.5, True, ["Pad Thai", "Tom Yum Soup"],
-                         ["Chef Somsak", "Somsak Boonkam", "5'7", "Le Cordon Bleu"])
+restaurant2 = Restaurant(
+    "Green Lotus", "Thai", "$", 40, "11AM", "11PM", "456 High St", 4.5, True,
+    ["Pad Thai", "Tom Yum Soup"],
+    ["Chef Somsak", "Somsak Boonkam", "5'7", "Le Cordon Bleu"])
 
-restaurant3 = Restaurant("Sunset", "Indian", "$", 60, "11AM", "11PM",
-                         "789 Broadway", 4.8, True, ["Biryani", "Butter Chicken"],
-                         ["Chef Sharma", "Ramesh Sharma", "6'0", "Institute of Hotel Management, Catering & Nutrition, Pusa"])
+restaurant3 = Restaurant(
+    "Sunset", "Indian", "$", 60, "11AM", "11PM", "789 Broadway", 4.8, True,
+    ["Biryani", "Butter Chicken"], [
+        "Chef Sharma", "Ramesh Sharma", "6'0",
+        "Institute of Hotel Management, Catering & Nutrition, Pusa"
+    ])
 
-restaurant4 = Restaurant("Italian Star", "Italian", "$$$", 30, "5PM", "11PM",
-                         "321 Geo St", 4.3, False, ["Pasta", "Pizza"],
-                         ["Chef Martini", "Gianluca Martini", "5'8", "ALMA Scuola di Cucina"])
+restaurant4 = Restaurant(
+    "Italian Star", "Italian", "$$$", 30, "5PM", "11PM", "321 Geo St", 4.3,
+    False, ["Pasta", "Pizza"],
+    ["Chef Martini", "Gianluca Martini", "5'8", "ALMA Scuola di Cucina"])
 
-restaurant5 = Restaurant("Samurai House", "Japanese", "$$$$$", 100, "5PM",
-                         "10PM", "654 Call Rd", 4.9, True, ["Sushi", "Ramen"],
-                         ["Chef Yamada", "Yamada Taro", "5'5", "Tsukiji Cooking School"])
+restaurant5 = Restaurant(
+    "Samurai House", "Japanese", "$$$$$", 100, "5PM", "10PM", "654 Call Rd",
+    4.9, True, ["Sushi", "Ramen"],
+    ["Chef Yamada", "Yamada Taro", "5'5", "Tsukiji Cooking School"])
 
 restaurant6 = Restaurant("Pepito's", "Mexican", "$", 50, "11AM", "10PM",
-                         "987 Park St", 4.1, True, ["Tacos", "Burritos"],
-                         ["Chef Gonzalez", "Jose Gonzalez", "5'8", "Escuela de Gastronomía Mexicana"])
+                         "987 Park St", 4.1, True, ["Tacos", "Burritos"], [
+                             "Chef Gonzalez", "Jose Gonzalez", "5'8",
+                             "Escuela de Gastronomía Mexicana"
+                         ])
 
-restaurant7 = Restaurant("La Gourmandise", "French", "$$$$", 20, "6PM", "12AM",
-                         "951 Aria Rd", 4.7, False, ["Foie Gras", "Croissant"],
-                         ["Chef Lefevre", "Jean Lefevre", "6'1", "École Grégoire-Ferrandi"])
+restaurant7 = Restaurant(
+    "La Gourmandise", "French", "$$$$", 20, "6PM", "12AM", "951 Aria Rd", 4.7,
+    False, ["Foie Gras", "Croissant"],
+    ["Chef Lefevre", "Jean Lefevre", "6'1", "École Grégoire-Ferrandi"])
 
-restaurant8 = Restaurant("Athenian", "Greek", "$$", 75, "11AM", "11PM",
-                         "521 Metro St", 4.4, True, ["Gyros", "Moussaka"],
-                         ["Chef Papadopoulos", "George Papadopoulos", "5'10", "Le Monde Institute"])
+restaurant8 = Restaurant(
+    "Athenian", "Greek", "$$", 75, "11AM", "11PM", "521 Metro St", 4.4, True,
+    ["Gyros", "Moussaka"],
+    ["Chef Papadopoulos", "George Papadopoulos", "5'10", "Le Monde Institute"])
 
-restaurant9 = Restaurant("Viking", "Scandinavian", "$$$$", 10, "6PM", "1AM",
-                         "846 Fjord St", 4.6, False, ["Gravlax", "Swedish Meatballs"],
-                         ["Chef Thomsen", "Ottis Thomsen", "6'2", "Oslo Culinary Academy"])
+restaurant9 = Restaurant(
+    "Viking", "Scandinavian", "$$$$", 10, "6PM", "1AM", "846 Fjord St", 4.6,
+    False, ["Gravlax", "Swedish Meatballs"],
+    ["Chef Thomsen", "Ottis Thomsen", "6'2", "Oslo Culinary Academy"])
 
-restaurant10 = Restaurant("Herbs & Spice", "Vegetarian", "$$", 80, "11AM",
-                          "11PM", "213 Main St", 4.2, True, ["Salad", "Veggie Burger"],
-                          ["Chef Green", "Emma Green", "5'9", "Natural Gourmet Institute"])
+restaurant10 = Restaurant(
+    "Herbs & Spice", "Vegetarian", "$$", 80, "11AM", "11PM", "213 Main St",
+    4.2, True, ["Salad", "Veggie Burger"],
+    ["Chef Green", "Emma Green", "5'9", "Natural Gourmet Institute"])
 
-restaurant11 = Restaurant("Burger Palace", "American", "$$$$", 50, "11AM",
-                          "11AM", "123 Main St", 2.5, True, ["Burger", "Fries"],
-                          ["Chef Johnson", "John Johnson", "5'11", "Culinary Institute of America"])
+restaurant11 = Restaurant(
+    "Burger Palace", "American", "$$$$", 50, "11AM", "11AM", "123 Main St",
+    2.5, True, ["Burger", "Fries"],
+    ["Chef Johnson", "John Johnson", "5'11", "Culinary Institute of America"])
 
-restaurant12 = Restaurant("Veggie Grill", "Vegetarian", "$$", 50, "11AM",
-                          "11PM", "123 Main St", 4.5, True, ["Burger", "Fries"],
-                          ["Chef Patel", "Anita Patel", "5'5", "Natural Gourmet Institute"])
+restaurant12 = Restaurant(
+    "Veggie Grill", "Vegetarian", "$$", 50, "11AM", "11PM", "123 Main St", 4.5,
+    True, ["Burger", "Fries"],
+    ["Chef Patel", "Anita Patel", "5'5", "Natural Gourmet Institute"])
 
-restaurant13 = Restaurant("Veggie Heaven", "Vegetarian", "$$$$$", 50, "11AM",
-                          "11PM", "123 Main St", 4.5, True, ["Burger", "Fries"],
-                          ["Chef Kim", "Min-Jae Kim", "5'7", "Natural Gourmet Institute"])
+restaurant13 = Restaurant(
+    "Veggie Heaven", "Vegetarian", "$$$$$", 50, "11AM", "11PM", "123 Main St",
+    4.5, True, ["Burger", "Fries"],
+    ["Chef Kim", "Min-Jae Kim", "5'7", "Natural Gourmet Institute"])
 
-restaurant14 = Restaurant("Veggie House", "Vegetarian", "$$$", 50, "11AM",
-                          "11PM", "123 Main St", 4.5, True, ["Burger", "Fries"],
-                          ["Chef Brown", "Oliver Brown", "6'1", "Natural Gourmet Institute"])
+restaurant14 = Restaurant(
+    "Veggie House", "Vegetarian", "$$$", 50, "11AM", "11PM", "123 Main St",
+    4.5, True, ["Burger", "Fries"],
+    ["Chef Brown", "Oliver Brown", "6'1", "Natural Gourmet Institute"])
 
-restaurant15 = Restaurant("Burger King", "American", "$$", 50, "11AM", "11PM",
-                          "123 Main St", 4.5, True, ["Burger", "Fries"],
-                          ["Chef Smith", "William Smith", "5'10", "Culinary Institute of America"])
-
+restaurant15 = Restaurant(
+    "Burger King", "American", "$$", 50, "11AM", "11PM", "123 Main St", 4.5,
+    True, ["Burger", "Fries"],
+    ["Chef Smith", "William Smith", "5'10", "Culinary Institute of America"])
 
 restaurants = [
     restaurant1, restaurant2, restaurant3, restaurant4, restaurant5,
@@ -234,12 +257,12 @@ restaurants = [
     restaurant11, restaurant12, restaurant13, restaurant14, restaurant15
 ]
 
-print(do_deliver(restaurants))
+print(Restaurant.do_deliver())
 print()
 print(restaurant7.hours_a_day_open())
 print()
 print(
-    f"The average rating for the restaurants on the list was {average_rating(restaurants):.2f}. It cloud have been better, but {restaurant11.name}'s a real piece of shit."
+    f"The average rating for the restaurants on the list was {average_rating(restaurants):.2f}. It could have been better, but {restaurant11.name}'s a real piece of shit."
 )
 print()
 print(f"The mean rating is {mean_rating(restaurants):.2f}.")
