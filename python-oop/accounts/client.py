a6652269b555e7ae3c2813c965a39f761a623853
rafael kasinski <rakasin@gmail.com>
2023-06-01 16:55:42 -0300

    Client setters for names and full_name

--- python-oop/accounts/client.py
+++ python-oop/accounts/client.py
@@ -20,3 +20,20 @@ class Client:
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+
+    # setter properties
+    @first_name.setter
+    def first_name(self, first_name):
+        print("calling first_name setter")
+        self.__first_name = first_name
+
+    @last_name.setter
+    def last_name(self, last_name):
+        print("calling last_name setter")
+        self.__last_name = last_name
+
+    @full_name.setter
+    def full_name(self, full_name):
+        first_name, last_name = full_name.split(" ", 1)
+        self.__first_name = first_name
+        self.__last_name = last_name
