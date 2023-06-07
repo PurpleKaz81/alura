98a1123c92c85c35240f26b33bcdf3ccefae024f
rafael kasinski <rakasin@gmail.com>
2023-06-07 16:13:22 -0300

    Added like method an looped it with range

--- python-oop-2/model.py
+++ python-oop-2/model.py
@@ -1,17 +1,29 @@
 class Movie:
     def __init__(self, name, year, length):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.length = length
+        self.likes = 0
+
+    def add_like(self):
+        self.likes += 1
 
 class Series:
     def __init__(self, name, year, seasons):
-        self.name = name
+        self.name = name.title()
         self.year = year
         self.seasons = seasons
+        self.likes = 0
+
+    def add_like(self):
+        self.likes += 1
 
 avengers = Movie("Avengers", 2009, 160)
-print(avengers.name)
+for _ in range(3):
+    avengers.add_like()
+print(f"{avengers.name} is a movie from {avengers.year} with a {avengers.length}-minute runtime. Likes: {avengers.likes}", "\n")
 
 sopranos = Series("Sopranos", 1999, 6)
-print(f"{sopranos.name} started in {sopranos.year} and ran for {sopranos.seasons} seasons.")
+for _ in range(2):
+    sopranos.add_like()
+print(f"{sopranos.name} started in {sopranos.year} and ran for {sopranos.seasons} seasons. Likes: {sopranos.likes}", "\n")
