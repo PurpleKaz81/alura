79eef32de2e73b0d9394e451ed0f69cbcad0b815
rafael kasinski <rakasin@gmail.com>
2023-06-07 16:31:27 -0300

    Encapsultation, getters, and setters done

--- python-oop-2/model.py
+++ python-oop-2/model.py
@@ -1,29 +1,81 @@
 class Movie:
     def __init__(self, name, year, length):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.length = length
-        self.likes = 0
+        self.__likes = 0
 
-    def add_like(self):
-        self.likes += 1
+    #getter properties
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    #setter properties
+    @name.setter
+    def name(self, name):
+        if name is None or name == "":
+            raise ValueError("Gotta have a name!")
+        else:
+            return self.__name
+
+    @likes.setter
+    def likes(self, amount):
+        if amount is None or amount == "":
+            raise ValueError("Give us an actual amount")
+        elif amount < 0:
+            raise ValueError("There are no negative likes")
+        else:
+            return self.__likes
+
+    #instance methods
+    def add_like(self, occurrences):
+        self.__likes += occurrences
 
 class Series:
     def __init__(self, name, year, seasons):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.seasons = seasons
-        self.likes = 0
+        self.__likes = 0
 
-    def add_like(self):
-        self.likes += 1
+    #getter properties
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    #setter properties
+    @name.setter
+    def name(self, name):
+        if name is None or name == "":
+            raise ValueError("Gotta have a name!")
+        else:
+            return self.__name
+
+    @likes.setter
+    def likes(self, amount):
+        if amount is None or amount == "":
+            raise ValueError("Give us an actual amount")
+        elif amount < 0:
+            raise ValueError("There are no negative likes")
+        else:
+            return self.__likes
+
+    #instance methods
+    def add_like(self, occurrences):
+        self.__likes += occurrences
 
 avengers = Movie("Avengers", 2009, 160)
-for _ in range(3):
-    avengers.add_like()
+avengers.add_like(3)
 print(f"{avengers.name} is a movie from {avengers.year} with a {avengers.length}-minute runtime. Likes: {avengers.likes}", "\n")
 
 sopranos = Series("Sopranos", 1999, 6)
-for _ in range(2):
-    sopranos.add_like()
+sopranos.add_like(2)
 print(f"{sopranos.name} started in {sopranos.year} and ran for {sopranos.seasons} seasons. Likes: {sopranos.likes}", "\n")
