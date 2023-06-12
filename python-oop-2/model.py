7f4be7147f890638450b98edc0d93f3b7021af33
rafael kasinski <rakasin@gmail.com>
2023-06-12 16:36:21 -0300

    New getter for id and new variable for tracking it. Id now private

--- python-oop-2/model.py
+++ python-oop-2/model.py
@@ -1,9 +1,12 @@
 class Watchable:
+    #class variable for tracking id's
+    _id_tracker = 0
 
     def __init__(self, name, year):
         self._name = name.title()
         self.year = year
         self._likes = 0
+        self._id = self.new_id()
 
     #dunder methods
     def __str__(self, name, year):
@@ -36,27 +39,22 @@ class Watchable:
             raise ValueError("Can't decrement likes.")
         self.likes += occurrences
 
-    #instance methods
-    def add_like(self, occurrences):
-        if occurrences < 0:
-            raise ValueError("Can't decrement likes.")
-        new_likes = self._likes + occurrences
-        self.likes = new_likes
+    @property
+    def id(self):
+        return self._id
+
+    #class methods
+    @classmethod
+    def new_id(cls):
+        cls._id_tracker += 1
+        return cls._id_tracker
 
 
 class Movie(Watchable):
-    # class variable
-    _id = 0
 
     def __init__(self, name, year, length):
         super().__init__(name, year)
         self.length = length
-        self.id = Movie.new_id()
-
-    @classmethod
-    def new_id(cls):
-        cls._id += 1
-        return cls._id
 
 
 class Series(Watchable):
@@ -83,18 +81,18 @@ avengers = Movie("Avengers", 2009, 160)
 avengers.add_like(3)
 avengers.name = "the AveNgers"
 print(
-    f"{avengers.name} (ID: {avengers._id}) is a movie from {avengers.year} with a {avengers.length}-minute runtime. Likes: {avengers.likes}",
+    f"{avengers.name} (Movie ID: {avengers.id}) is a movie from {avengers.year} with a {avengers.length}-minute runtime. Likes: {avengers.likes}",
     "\n")
 
 sopranos = Series("Sopranos", 1999, 6)
 sopranos.add_like(2)
 print(
-    f"{sopranos.name} started in {sopranos.year} and ran for {SmallNumberFormatter.format_small_numbers(sopranos.seasons)} seasons. Likes: {sopranos.likes}",
+    f"{sopranos.name} (Series ID: {sopranos.id}) started in {sopranos.year} and ran for {SmallNumberFormatter.format_small_numbers(sopranos.seasons)} seasons. Likes: {sopranos.likes}",
     "\n")
 
 sentinelle = Movie("Sentinelle", 1992, 139)
 sentinelle.add_like(0)
 sentinelle.name = "la sentinelLe"
 print(
-    f"{sentinelle.name} (ID: {sentinelle._id}) is a movie from {sentinelle.year} with a {sentinelle.length}-minute runtime. It has {sentinelle.likes} likes cuz it's a piece of shit.",
+    f"{sentinelle.name} (Movie ID: {sentinelle.id}) is a movie from {sentinelle.year} with a {sentinelle.length}-minute runtime. It has {sentinelle.likes} likes cuz it's trash.",
 )
