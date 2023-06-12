2af28a35e3cdabf8f9379e2dc8f1f58ddf56f663
rafael kasinski <rakasin@gmail.com>
2023-06-12 15:19:11 -0300

    Added superclass and method

--- python-oop-2/model.py
+++ python-oop-2/model.py
@@ -1,85 +1,56 @@
-class Movie:
+class Watchable:
+
+    def __init__(self, name, year):
+        self.__name = name.title()
+        self.year = year
+        self.__likes = 0
+
+    #getter properties
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    #setter properties
+    @name.setter
+    def name(self, new_name):
+        if new_name is None or new_name == "":
+            raise ValueError("Gotta have a name!")
+        else:
+            self.__name = new_name.title()
+
+    @likes.setter
+    def likes(self, new_amount):
+        if not isinstance(new_amount, int):
+            raise TypeError("Amount of likes needs to be an integer.")
+        elif new_amount < 0:
+            raise ValueError("There are no negative likes.")
+        else:
+            self.__likes = new_amount
+
+    #instance methods
+    def add_like(self, occurrences):
+        if occurrences < 0:
+            raise ValueError("Can't decrement likes.")
+        new_likes = self.__likes + occurrences
+        self.likes = new_likes
+
+
+class Movie(Watchable):
 
     def __init__(self, name, year, length):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.length = length
-        self.__likes = 0
-
-    #getter properties
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    #setter properties
-    @name.setter
-    def name(self, new_name):
-        if new_name is None or new_name == "":
-            raise ValueError("Gotta have a name!")
-        else:
-            self.__name = new_name.title()
-
-    @likes.setter
-    def likes(self, new_amount):
-        if not isinstance(new_amount, int):
-            raise TypeError("Amount of likes needs to be an integer.")
-        elif new_amount < 0:
-            raise ValueError("There are no negative likes.")
-        else:
-            self.__likes = new_amount
-
-    #instance methods
-    def add_like(self, occurrences):
-        if occurrences < 0:
-            raise ValueError("Can't decrement likes.")
-        new_likes = self.__likes + occurrences
-        self.likes = new_likes
 
 
-class Series:
+class Series(Watchable):
 
     def __init__(self, name, year, seasons):
-        self.__name = name.title()
-        self.year = year
+        super().__init__(name, year)
         self.seasons = seasons
-        self.__likes = 0
-
-    #getter properties
-    @property
-    def name(self):
-        return self.__name
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    #setter properties
-    @name.setter
-    def name(self, new_name):
-        if new_name is None or new_name == "":
-            raise ValueError("Gotta have a name!")
-        else:
-            self.__name = new_name.title()
-
-    @likes.setter
-    def likes(self, new_amount):
-        if not isinstance(new_amount, int):
-            raise TypeError("Amount of likes needs to be an integer.")
-        elif new_amount < 0:
-            raise ValueError("There are no negative likes.")
-        else:
-            self.__likes = new_amount
-
-    #instance methods
-    def add_like(self, occurrences):
-        if occurrences < 0:
-            raise ValueError("Can't decrement likes.")
-        new_likes = self.__likes + occurrences
-        self.likes = new_likes
 
     #static methods
     @staticmethod
@@ -97,6 +68,7 @@ class Series:
             10: 'ten'
         }
         return numbers_to_words.get(number, number)
+
 
 avengers = Movie("Avengers", 2009, 160)
 avengers.add_like(3)
