991ba8cb5392549b7cf7c7d43c875112d137c215
rafael kasinski <rakasin@gmail.com>
2023-06-19 14:37:05 -0300

    Created new main.py to use find method

---
+++ url_extractor/old_main.py
@@ -0,0 +1,30 @@
+def print_with_newline(*args):
+    print(*args, end="\n\n")
+
+url = "https://bytebank.com/cambio?quantidade=190&moedaOrigem=real&moedaDestino=dolar"
+
+url_length = len(url)
+url_parts = url.split("?")
+url_base = url_parts[0]
+url_parameters = url_parts[1]
+
+parameters = url_parameters.split("&")
+
+origin, destination, quantity = None, None, None
+
+for param in parameters:
+    key, value = param.split("=")
+    if key == "moedaOrigem":
+        origin = value
+    elif key == "moedaDestino":
+        destination = value
+    elif key == "quantidade":
+        quantity = value
+
+print_with_newline("URL:", url)
+print_with_newline(f"URL length: {url_length} characters")
+print_with_newline("URL parts:", ", ".join(url_parts))
+print_with_newline("URL parameters:", ", ".join(parameters))
+print_with_newline("Origin currency:", origin)
+print_with_newline("Destination currency:", destination)
+print_with_newline("Quantity to convert:", quantity)
