94b11712dba1f2f559b54f16af14d81105007f61
rafael kasinski <rakasin@gmail.com>
2023-06-20 16:06:54 -0300

    Refactor main.py to use variable for URL input

--- url_extractor/main.py
+++ url_extractor/main.py
@@ -1,5 +1,6 @@
 from url_extractor import URLExtractor as Extractor
 
-extracted_parameters = Extractor("https://bytebank.com/cambio?quantidade=190&moedaOrigem=real&moedaDestino=dolar")
+url = "https://bytebank.com/cambio?quantidade=190&moedaOrigem=real&moedaDestino=dolar"
+extracted_parameters = Extractor(url)
 quantity_parameter = extracted_parameters.get_parameter("quantidade")
 quantity = extracted_parameters.print_with_newline(quantity_parameter)
