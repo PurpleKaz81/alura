7d5718a1b861cbce25f3a045dabaae6be0bb4550
rafael kasinski <rakasin@gmail.com>
2023-07-04 18:24:12 -0300

    Add deposit_all_accounts and fix_name methods in CheckingAccount class. Finished 2/8

--- python-lists-tuples/main_2.py
+++ python-lists-tuples/main_2.py
@@ -8,24 +8,33 @@ class CheckingAccount:
         self.balance += value
 
     def __str__(self):
-        regular_return = f"Account number {self.account_number} has a balance of {self.format_values(self.balance)}."
-        rich_return = f"Account number {self.account_number} has a balance of {self.format_values(self.balance)}. He's rich, biatch!"
-        return regular_return if self.balance < 5000 else rich_return
+        return (
+            f"Account number {self.account_number} has a balance of {self.format_values(self.balance)}."
+            " He's rich, biatch!" if self.balance >= 5000 else
+            f"Account number {self.account_number} has a balance of {self.format_values(self.balance)}."
+        )
 
     @staticmethod
     def format_values(value):
         return f"US${value:,.2f}"
 
+    @staticmethod
+    def deposit_all_accounts(accounts):
+        for account in accounts:
+            account.deposit(100)
+
+    @staticmethod
+    def fix_name(users):
+        return [
+            ("Rafael Kasinski", ) + user[1:] if user[0] == "Rafael" else user
+            for user in users
+        ]
+
 
 account_1 = CheckingAccount("616")
 account_1.deposit(6666.66)
-# print(account_1)
-# print()
-
 account_2 = CheckingAccount("1818")
 account_2.deposit(1818.18)
-# print(account_2)
-# print()
 
 accounts = [account_1, account_2]
 
@@ -33,3 +42,24 @@ for account in accounts:
     # print(f"Account {account.account_number} has a balance of {CheckingAccount.format_values(account.balance)}.")
     print(account)
     print()
+
+CheckingAccount.deposit_all_accounts(accounts)
+for account in accounts:
+    print(account)
+    print()
+
+rick = ("Richard", 67, 1956)
+rafael = ("Rafael", 42, 1981)
+print(rafael)
+print()
+rafael = CheckingAccount.fix_name([rafael])[0]
+print(rafael)
+print()
+
+paul = ("Paul", 22, 2002)
+users = (rick, rafael, paul)
+for user in users:
+    print(', '.join(map(str, user)))
+    print()
+
+print(', '.join(map(str, users[0])))
