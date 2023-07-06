1141001638dcb44cf10538899403fee2148de305
rafael kasinski <rakasin@gmail.com>
2023-07-05 21:00:49 -0300

    Add comparison methods, balance getter, and richest account finder to WageAccount class

--- python-lists-tuples/main_4.py
+++ python-lists-tuples/main_4.py
@@ -1,11 +1,15 @@
 from main_3 import Account, CheckingAccount, SavingsAccount, InvestmentAccount
+from operator import attrgetter
 
 
 class WageAccount:
 
+    _accounts = []
+
     def __init__(self, account_number):
         self._account_number = account_number
         self._balance = 0
+        self.__class__._accounts.append(self)
 
     def deposit(self, value):
         self._balance += value
@@ -24,9 +28,23 @@ class WageAccount:
 
         return self._account_number == other._account_number and self._balance == other._balance
 
+    def __lt__(self, other):
+        return self._balance < other._balance
+
+    def __gt__(self, other):
+        return self._balance > other._balance
+
+    @property
+    def get_balance(self):
+        return self._balance
+
     @staticmethod
     def format_values(value):
         return f"US${value:,.2f}"
+
+    @classmethod
+    def richest_mf(cls):
+        return max(cls._accounts, key=attrgetter("get_balance"))
 
 
 class WageMultiple(WageAccount):
@@ -60,3 +78,24 @@ print(isinstance(SavingsAccount(99), SavingsAccount))
 print(isinstance(WageMultiple(81), Account))
 
 print(isinstance(SavingsAccount(191), Account))
+print()
+
+account_5 = WageAccount(5)
+account_5.deposit(500)
+
+account_6 = WageAccount(10)
+account_6.deposit(10000)
+
+account_7 = WageAccount(83)
+account_7.deposit(879)
+
+accounts = [account_5, account_6, account_7]
+
+for account in sorted(accounts):
+    print(account)
+
+print()
+
+print(
+    f"Mirror mirror on the wall, who's the richest of them all? It's Account number {WageAccount.richest_mf()._account_number}!"
+)
